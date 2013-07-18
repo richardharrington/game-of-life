81361bbabf1f7b27ff966c1393ba28b0c06e862d
jimmyli <jyl702@gmail.com>
2013-07-18 15:30:43 -0400

    got basic functions set up

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,6 +1,43 @@
 (ns game-of-life.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(use '[clojure.set :only union difference])
+
+(def live-cells-test #{[2 1] [2 3] [0 0] [1 3]})
+
+(defn neighbors [[x y]]
+	(disj (set (for [i (range (- x 1) (+ x 2))
+		  j (range (- y 1) (+ y 2))]
+		  [i j])) [x y]))
+
+(defn live-neighbors [cell, live-cells]
+	(set (filter live-cells (neighbors cell))))
+
+(defn dead-neighbors [cell, live-cells]
+	(difference (neighbors cell) (live-neighbors cell live-cells)))
+
+(defn num-live-neighbors
+	"Takes in a cell (coordinate pair),
+	and the complete set of ACTIVE cells, return the num of ACTIVE neighbors"
+	[cell, live-cells]
+	(count (live-neighbors cell live-cells)))
+
+(defn num-dead-neighbors
+	[cell, live-cells]
+	(count (dead-neighbors cell live-cells)))
+
+(defn stay-alive? [cell live-cells]
+	(#{2 3} (num-live-neighbors cell live-cells)))
+
+(defn come-alive? [cell live-cells]
+	(= 3 (num-live-neighbors cell live-cells)))
+
+(defn next-live-cells [live-cells]
+	(let [all-dead-neighbors (apply union (map #(dead-neighbors % live-cells) live-cells))]
+		(set (concat
+			(filter #(come-alive? % live-cells) all-dead-neighbors)
+			(filter #(stay-alive? % live-cells) live-cells)))))
+
+; X # # #
+; # # X # 
+; # # # #
+; # X X #
