de6e01ce1803fd2a1b59d8e86bc50a69c35650e5
Richard Harrington <harrington.richard@gmail.com>
2013-07-19 15:31:04 -0400

    solved 4clojure game of life

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,37 +1,27 @@
 (ns game-of-life.core)
 
-(use '[clojure.string :only [join]])
-
-(def live-cells-test #{[2 1] [2 3] [0 0] [1 3] [2 2] [3 3]})
-
-
-
-(defn next-live-cells [live-cells]
-  (let [neighbors (fn [[x y]]
-                    (disj (set (for [i (range (- x 1) (+ x 2))
-                                     j (range (- y 1) (+ y 2))]
-                                 [i j])) 
-                          [x y]))
-        count-live-neighbors-matches (fn [pred]
-                                       #(pred (count (filter live-cells (neighbors %)))))
-        dead-cells-nearby (filter (complement live-cells) 
-                                       (distinct (mapcat neighbors live-cells)))]
-    (set (concat
-           (filter (count-live-neighbors-matches #{2 3}) live-cells)
-           (filter (count-live-neighbors-matches #{3}) dead-cells-nearby)))))
-
-(defn print-cells [live-cells width height]
-  (println (apply str (repeat width "-")))
-  (println (join "\n" (for [y (range height)]
-                        (apply str (for [x (range width)]
-                                     (if (live-cells [x y])
-                                       "#"
-                                       " ")))))))
-
-(defn play [initial-live-cells width height millisecs]
-  (loop [live-cells initial-live-cells]
-    (when (not-empty live-cells)
-      (print-cells live-cells width height)
-      (Thread/sleep millisecs)
-      (recur (next-live-cells live-cells)))))
+(def f (fn 
+         "This function passes the game-of-life tests on 4clojure."
+         [grid]
+         (let [live-cells (set (for [y (range (count grid)), x (range (count (grid y))) :when (= (get-in grid [y x]) \#)]
+                                 [x y]))
+               height (count grid)
+               width (count (grid 0))
+               neighbors (fn [[x y]]
+                           (disj (set (for [i (range (- x 1) (+ x 2))
+                                            j (range (- y 1) (+ y 2))]
+                                        [i j])) 
+                                 [x y]))
+               count-live-neighbors-matches (fn [pred]
+                                              #(pred (count (filter live-cells (neighbors %)))))
+               dead-cells-nearby (filter (complement live-cells) 
+                                         (distinct (mapcat neighbors live-cells)))
+               next-live-cells (set (concat
+                                      (filter (count-live-neighbors-matches #{2 3}) live-cells)
+                                      (filter (count-live-neighbors-matches #{3}) dead-cells-nearby)))]
+           (for [y (range height)]
+             (apply str (for [x (range width)]
+                          (if (next-live-cells [x y])
+                            "#"
+                            " ")))))))
 
